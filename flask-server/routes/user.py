ad44f03f5575c6295677888f28fadad64e6c048e
Thato Matlala <thatomatlala28@gmail.com>
2024-10-09 15:24:24 +0200

    New login

--- flask-server/routes/user.py
+++ flask-server/routes/user.py
@@ -1,15 +1,26 @@
 from flask import Blueprint, jsonify, request
-from extensions import db
+from extensions import bcrypt
 from models.user import User
 
-Persona_bp = Blueprint("Persona", __name__)
+user_bp = Blueprint("user", __name__)
 
-@Persona_bp.route("/login", methods=["GET", "POST"])
+
+@user_bp.route("/login", methods=["GET", "POST"])
 def login():
     body = request.get_json()
-    user = User.query.filter_by(user_Ecode=body.user_Ecode).first()
+    print(body)
+    user = User.query.filter_by(user_Ecode=body["user_Ecode"]).first()
     if user:
-        return jsonify({'message':"Success","user":user})
+        if bcrypt.check_password_hash(user.password_hash, body["password"]):
+            return jsonify(
+                {
+                    "message": "Success",
+                    "user_id": user.id,
+                    "name": user.user_name,
+                    "surname": user.user_lastname,
+                }
+            )
+        else:
+            return jsonify({"message": "Failed: Invalid Ecode/password"}), 401
     else:
-        return jsonify({'message': 'Failed'})
-
+        return jsonify({"message": "Failed: Invalid Ecode/password"}), 401
